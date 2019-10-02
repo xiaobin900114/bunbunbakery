fd031e53f3c40735327f22fa5501ce0eaadbc419
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-10-01 20:28:32 -0400

    detail enhancement and images updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,39 +6,53 @@ const count = document.getElementById("count").children;
 const test = document.getElementById("test");
 const blackback = document.getElementById("blackout");
 const orderInPrdPage = document.getElementsByClassName("order_btn");
-
-console.log(glazing.length);
-console.log(count.length);
-console.log(orderInPrdPage);
-
 const slideInPage = document.getElementById("slide-in-page");
+
+//testing vars
+
+//console.log(glazing.length);
+//console.log(count.length);
+//console.log(orderInPrdPage);
+
+//test the trigger button on the prd list page
+/* triggerBtn.addEventListener("click", function(){
+  blackout.style.visibility = "visible";
+  slideInPage.style.visibility = "visible";
+  slideInPage.style.right = "0";
+}) */
+
+
+//add eventlistener to all the order buttons on the product list page
 
 for(let i=0; i < orderInPrdPage.length; i++) {
   orderInPrdPage[i].addEventListener("click", function(){
     blackout.style.visibility = "visible";
     slideInPage.style.visibility = "visible";
+    slideInPage.style.right = "0";
   })
 }
 
-triggerBtn.addEventListener("click", function(){
-  blackout.style.visibility = "visible";
-  slideInPage.style.visibility = "visible";
-})
+//add eventlistener to order_btn on detail page
 
 triggerClose.addEventListener("click", function() {
   slideInPage.style.visibility = "hidden";
   blackout.style.visibility = "hidden";
 })
 
+//add eventlistener to blk blackground to close the slide-in window
+
 blackback.addEventListener("click", function() {
   slideInPage.style.visibility = "hidden";
   blackout.style.visibility = "hidden";
 })
 
-test.addEventListener("click", function() {
+//test the eventlistener on selecting different glazings and count
+/*test.addEventListener("click", function() {
   test.classList.remove("order-options");
   test.classList.add("order-options-clicked");
-})
+})*/
+
+//add eventlisteners to glazing and count selections
 
 for(let i=0;i < glazing.length ;i++) {
   glazing[i].addEventListener("click",function(){
